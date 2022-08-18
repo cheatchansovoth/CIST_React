459246260b4c8eb668cc9dbb1541f96e46572ba1
cheatchansovoth <57830826+cheatchansovoth@users.noreply.github.com>
2022-08-18 22:20:12 +1200

    Updated

---
+++ cise_ass1a_worksheet4/frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article";
+import { Route, NavLink, BrowserRouter as Router } from "react-router-dom";
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+        <ul className="header">
+          <li>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/SEPractice">Select the Practice</NavLink>
+          </li>
+          <li>
+            <NavLink to="/SubmitArticle">Submit an Article</NavLink>
+          </li>
+        </ul>
+        <div className="content">
+          <Route exact path="/" component={Home} />
+          <Route path="/SEPractice" component={SEPractice} />
+          <Route path="/SubmitArticle" component={SubmitArticle} />
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
